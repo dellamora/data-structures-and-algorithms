0a738f8ab49a88330b73b5dcc5c28ac9a3776fd2
Dellamora <francielle@dellamora.dev>
2023-10-01 20:00:39 -0300

    first commit

---
+++ index.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func helloWolrd() {
+	fmt.Println("hello world")
+}
+
+func add(x, y int) {
+	total := 0
+	total = x + y
+	fmt.Println(total)
+}
+
+func sum_char_codes(n string) {
+	sum := 0
+	for i := 0; i < len(n); i++ {
+		sum += int(n[i])
+	}
+
+	fmt.Println(sum)
+}
+
+func main() {
+	// helloWolrd()
+	// add(15, 35)
+	sum_char_codes("francielle")
+}
