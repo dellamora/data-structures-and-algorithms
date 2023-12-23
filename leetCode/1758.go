ef103b5838af2c4cffd0a5dc54af70cfe6bc2e23
Dellamora <francielle@dellamora.dev>
2023-12-23 20:44:46 -0300

    feat: Daily Coding Challenge Completed!

---
+++ leetCode/1758.go
@@ -0,0 +1,27 @@
+package main
+
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}	
+
+func minOperations(s string) int {
+	var count int
+	for i, v := range s {
+		if i % 2 == 0 && v == '1' {
+			count++
+		} else if i % 2 == 1 && v == '0' {
+			count++
+		}
+	}
+	return min(count, len(s) - count)
+}
+
+func main() {
+	s := "0100"
+	minOperations(s)	
+	println(minOperations(s))
+}
