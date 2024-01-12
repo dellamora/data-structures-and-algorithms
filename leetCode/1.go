0864d5081bfcfee53192cda0e3e11d60cc3ba735
Dellamora <franciellle@dellamora.dev>
2024-01-11 21:50:55 -0300

    two sum

---
+++ leetCode/1.go
@@ -0,0 +1,10 @@
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+	for j := i+1; j < len(nums); i++ {
+		 if nums[i] + nums[j] == target {
+			return []int{i,j}
+		 }
+	}
+}
+return []int{}
+}
